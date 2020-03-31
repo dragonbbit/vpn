56668727c0395bd35475cc351c5963c494ca7870
geesimon <geesimon@hotmail.com>
2020-04-01 02:37:47 +0800

    Add docker setup content

--- install.sh
+++ install.sh
@@ -10,28 +10,6 @@ Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
-### Get dommain name
-while :
-do
-    read -p "请输入域名：" domain_name
-    if [[ $(echo -n $domain_name | wc -m) -gt 0 ]]; then        
-        break
-    fi
-    echo -e ${Error} "域名输入错误"
-done
-while :
-do
-    read -p "请输入口令：" password
-    if [[ $(echo -n $password | wc -m) -gt 0 ]]; then        
-        break
-    fi
-    echo -e ${Error} "口令输入错误"
-done
-
-v2ray_client_id=$(cat /proc/sys/kernel/random/uuid)
-
-### Install Docker (on CentOS)
-# 检查系统
 check_sys(){
     if [[ -f /etc/redhat-release ]]; then
         release="centos"
@@ -59,8 +37,17 @@ install_docker_on_centos() {
     sudo systemctl start docker
 }
 
-install_docker_on_ubuntu() {
-    echo 'install docker on ubuntu'    
+install_docker_on_ubuntu() {    
+    sudo apt-get remove -y docker docker-engine docker.io containerd runc
+    sudo apt-get update -y
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+    sudo systemctl enable docker
+    sudo systemctl start docker
 }
 
 install_docker() {
@@ -82,7 +69,58 @@ install_docker() {
     exec newgrp docker
 }
 
+get_info(){
+    while :
+    do
+        read -p $1 _info
+        if [[ $(echo -n $_info | wc -m) -gt 0 ]]; then        
+            break
+        fi
+        echo -e ${Error} "输入错误"
+    done
+}
+
+####################################
+### Install Docker               ###
+####################################
+
+get_info "请输入域名："
+domain_name=${_info}
+get_info "请输入口令："
+password=${_info}
+v2ray_client_id=$(cat /proc/sys/kernel/random/uuid)
+trojan_port=4443
+
 install_docker
+
+####################################
+### Setup Docker                 ###
+####################################
+
+### Build Cert Web
+mkdir -p ~/.cert
+docker build -t certweb --rm --build-arg DOMAIN_NAME=${domain_name} --build-arg V2RAY_CLIENT_ID=${v2ray_client_id} -f Dockerfile.certweb .
+docker run -d --name certweb_instance --restart=always -v ~/.cert:/etc/nginx/cert -p 80:80 -p 443:443 certweb
+
+### Build Trojan 
+certweb_addr=$(docker exec certweb_instance sh -c "/sbin/ip a" | grep -o "inet.*eth" | grep -o "[[:digit:]]\+.*\/")
+certweb_addr=${certweb_addr:0:-1}
+docker build -t trojan --build-arg PASSWORD=${password} --build-arg DOMAIN_NAME=${domain_name} --build-arg PORT=${trojan_port} --build-arg REMOTE_ADDR=${certweb_addr} --rm -f Dockerfile.trojan .
+docker run -d -v ~/.cert:/etc/trojan/cert -p ${trojan_port}:${trojan_port} --restart=always --name trojan_instance trojan
+
+### Build v2ray
+v2ray_client_id=$(docker exec v2ray_instance sh -c "cat /etc/v2ray/config.json" | egrep '"id"' | egrep -o '[0-9a-fA-F]{8}.*[0-9a-fA-F]{12}')
+docker build -t v2ray --rm --build-arg V2RAY_CLIENT_ID=${v2ray_client_id} -f Dockerfile.v2ray .
+docker run -d --name v2ray_instance --restart=always v2ray
+
+### Update web proxy address
+v2ray_addr=$(docker exec v2ray_instance sh -c "/sbin/ip a" | grep -o "inet.*eth" | grep -o "[[:digit:]]\+.*\/")
+v2ray_addr=${v2ray_addr:0:-1}
+docker exec -t certweb_instance sh -c "sed -i \"s~//.*:10000;~//${v2ray_addr}:10000;~\" /etc/nginx/conf.d/*.ssl.conf&&nginx -s reload"
+
+####################################
+### Show Info                    ###
+####################################
 
 echo -e "###################${Green_font_prefix}请记录${Font_color_suffix}###################"
 echo 
