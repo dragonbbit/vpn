ac2ec1af5a733f924077f04f4b373aa105fe2716
geesimon <geesimon@hotmail.com>
2020-04-03 12:11:49 -0400

    remove httpport since it's not what expected

--- cleanall.sh
+++ cleanall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker stop trojan_instance v2ray_instance cert_instance web_instance
-docker rm trojan_instance v2ray_instance cert_instance web_instance
-docker rmi trojan v2ray cert web
+docker stop trojan_instance v2ray_instance cert_instance web_instance speedtest_instance
+docker rm trojan_instance v2ray_instance cert_instance web_instance certweb_instance speedtest_instance
+docker rmi trojan v2ray cert web certweb  adolfintel/speedtest
 
