9550f502d7811a6e5537d68a06da8254e946ce4a
geesimon <geesimon@hotmail.com>
2020-04-22 15:24:06 +0800

    Add docker support for dnsmasq

--- cleanall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker stop trojan_instance v2ray_instance certweb_instance speedtest_instance
-docker rm trojan_instance v2ray_instance certweb_instance speedtest_instance
-docker rmi trojan v2ray certweb adolfintel/speedtest
-
