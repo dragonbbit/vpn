7a1545a69daefc0307d76a54e9f8519079d1a49b
dragonbbit <xj9@illinois.edu>
2020-04-23 16:55:59 +0000

    Fix bug

--- clean_v2ray_cdn.sh
+++ clean_v2ray_cdn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker stop v2ray_instance web_instance speedtest_instance
-docker rm v2ray_instance web_instance speedtest_instance
-docker rmi v2ray adolfintel/speedtest
+docker stop v2ray_instance web_instance speedtest_instance dnsmasq_instance
+docker rm v2ray_instance web_instance speedtest_instance dnsmasq_instance
+docker rmi v2ray adolfintel/speedtest dnsmasq
 
